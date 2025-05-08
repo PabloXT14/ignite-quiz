1ae769a679e952eadd52595e1090b8dbc0dd9201
Pablo Alan <pabloxt14@gmail.com>
2025-05-08 16:16:27 -0300

    feat: animate OverlayFeedback

--- src/components/OverlayFeedback/index.tsx
+++ src/components/OverlayFeedback/index.tsx
@@ -1,5 +1,12 @@
+import { useEffect } from 'react'
 import { useWindowDimensions } from 'react-native'
-import Animated from 'react-native-reanimated'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSequence,
+  withTiming,
+  Easing,
+} from 'react-native-reanimated'
 import { BlurMask, Canvas, Rect } from '@shopify/react-native-skia'
 
 import { THEME } from '../../styles/theme'
@@ -17,16 +24,33 @@ type Props = {
 export function OverlayFeedback({ status = 0 }: Props) {
   const { width, height } = useWindowDimensions()
 
+  const opacity = useSharedValue(0)
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+    }
+  })
+
   const color = STATUS[status]
+
+  useEffect(() => {
+    opacity.value = withSequence(
+      withTiming(1, { duration: 400, easing: Easing.bounce }),
+      withTiming(0)
+    )
+  }, [status])
 
   return (
     <Animated.View
-      style={{
-        width,
-        height,
-        position: 'absolute',
-        // zIndex: 1,
-      }}
+      style={[
+        {
+          width,
+          height: height + 40, // 40 is header height
+          position: 'absolute',
+        },
+        animatedStyle,
+      ]}
     >
       <Canvas style={{ flex: 1 }}>
         <Rect x={0} y={0} width={width} height={height} color={color}>
